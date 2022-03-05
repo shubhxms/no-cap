4b0d3d7034ae2334d02577e0826b71eb70474cf4
Shubham Shah <03.shubhamshah@gmail.com>
2022-03-06 01:54:23 +0530

    add why link for random and minor changes

--- src/About.js
+++ src/About.js
@@ -6,16 +6,16 @@ function About() {
 		  <h4>no-cap</h4>
 		  <p>
 			this tool will allow you to
-			<li>randomly capitalize</li>
+			<li>randomly capitalize <a href="https://www.reddit.com/r/OutOfTheLoop/comments/avyfl6/comment/ehip0xr/?utm_source=share&utm_medium=web2x&context=3">why?</a></li>
 			<li>upper case</li>
 			<li>lower case</li>
 			your text with one click (fr fr)
 		  </p>
 		  <h4>me</h4>
 		  <p>
-			  hi! I am <a href='https://twitter.com/shubhxms'>shubham</a>	
-			  <br/>
-			  lmk if you like it.	
+			  hi! I am <a href='https://twitter.com/shubhxms'>shubham</a>
+			  <br />
+			  made this for fun. lmk if you like it.	
 			  <br/>
 			  thx
 		  </p>
